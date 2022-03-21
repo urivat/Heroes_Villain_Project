ff8f20821656401f363f9b01b7d0bffcb8a244d2
rashad porter <rashad.porter18@gmail.com>
2022-03-21 17:13:51 -0400

    added the ability to group supers by type and finished crud endpoints

--- supers/views.py
+++ supers/views.py
@@ -14,61 +14,67 @@ class SupersList(APIView):
     def get(self, request, format=None):        
         queryset = Super.objects.all()
         super_type = request.query_params.get('type')
-        
+        types = SuperType.objects.all()
+        dictionary_holder = {}
+
+
         if super_type:
             queryset = queryset.filter(type__type=super_type)
-        serializer = SuperSerializer(queryset, many=True)
-        return Response(serializer.data)      
+            serializer = SuperSerializer(queryset, many=True)
+            return Response(serializer.data)
 
+        
+        for type in types:
+            supers = Super.objects.filter(type_id=type.id)
 
+            serializer = SuperSerializer(supers, many=True)
+            dictionary_holder[type.type]= {
+                'supers':serializer.data                
+            }
+        return Response(dictionary_holder)
+        
     def post(self, request, format=None):
         serializer = SuperSerializer(data = request.data)
+
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response (serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+        return Response (serializer.errors, status=status.HTTP_404_NOT_FOUND)    
+           
                          
-     
-            
     
-
-
-
+                  
+               
 class SupersDetails(APIView):
 
     def get_object(self, pk):
         try:
             return Super.objects.get(pk=pk)
+
         except Super.DoesNotExist:
             raise Http404
 
     def get(self,request,pk, format=None):
         
         supers =self.get_object(pk)
+
         serializer = SuperSerializer(supers)
         return Response(serializer.data)
 
     def put(self,request,pk, format=None):
         supers = self.get_object(pk)
         serializer=SuperSerializer(supers, data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk, format=None):
+
         super = self.get_object(pk)
-        super.delete
+        super.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class SuperTypesList(APIView):
-    def get(self,request,format=None):
-        types = SuperType.objects.all()
-        dictionary_holder ={}
-        for type in types:
-            supers = Super.objects.all(type_id=type.id)
-            serializer = SuperSerializer(supers, many=True)
-            dictionary_holder[supers_types.type]= {
-                'supers': serializer.data
-            }
-        return Response(dictionary_holder)
--- supers_types/views.py
+++ supers_types/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework import status
+from supers_types.models import SuperType
+from supers_types.serializer import SuperTypeSerializer
+from rest_framework.response import Response
+from django.http import Http404
 # Create your views here.
+class TypeList(APIView):
+    
+
+    def get(self, request, format=None):
+        type = SuperType.objects.all()
+        type_serializer = SuperTypeSerializer(type, many=True)
+        return Response(type_serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SuperTypeSerializer(request.data)
+        if serializer.is_valid(raise_exception=True, status=):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
